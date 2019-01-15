fe343dcc92f39fcc2ba9dcd1b778b76c7b7945e9
Taslim Oseni <taslimoseni@gmail.com>
2019-01-15 12:09:45 +0100

    Design done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,122 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="calculator">
+
+    <div class="title">Product Id</div>
+    <input class="tinput" v-model="message" placeholder="Enter Product ID">
+
+    <div class="title">Product Description</div>
+    <input class="tinput" v-model="message" placeholder="Enter Product Description">
+
+    <div class="title">Quantity</div>
+    <input class="tinput" v-model="message" placeholder="Enter Product Quantity">
+
+    <div class="title">Price</div>
+    <input class="tinput" v-model="message" placeholder="Enter Product Price">
+
+    <button class="butt">Add Product</button>
+
+    <table>
+      <h4>Available Products</h4>
+        <tr class="headoftable">
+            <th>ID</th>
+            <th>Description</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Actions</th>
+        </tr>
+    </table>
+
+    <table>
+      <h4>Your Shopping Cart</h4>
+        <tr class="headoftable">
+            <th>ID</th>
+            <th>Description</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Actions</th>
+        </tr>
+    </table>
+
+
+    <div class="total"><b>Grand Total: 0</b></div>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
   }
 }
+
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+.calculator{
+  margin-top: 40px;
+  margin-left: 280px;
+  margin-right: 300px;
 }
+
+.title{
+  font-size: 18px;
+  margin-top: 20px;
+}
+
+
+.tinput{
+  width: 100%;
+  margin-top: 10px;
+  height: 32px;
+  padding-left: 15px;
+  font-size: 15px;
+}
+
+
+.butt{
+  width: 120px;
+  height: 40px;
+  background-color: blue;
+  margin-top:40px;
+  color: white;
+  font-size: 16px;
+}
+
+table{
+  margin-top: 15px;
+}
+
+.headoftable{
+  background-color: black;
+  color: white;
+  height: 40px;
+}
+
+h4{
+  color: black;
+  display: inline-block;
+  font-size: 20px;
+  horizontal-align: middle;
+}
+
+th{
+  width: 200px;
+}
+
+.total{
+  font-size: 30px;
+  margin-top: 50px;
+  color: #000000;
+}
+
+
 </style>
